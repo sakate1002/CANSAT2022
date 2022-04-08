c7015dd091fe8ddb4eb799f9dd683b4612b00323
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-04-08 16:27:16 +0900

    Update a.py

--- a.py
+++ a.py
@@ -1,2 +1,13 @@
-print(1)
-aiaiaiai
+import random
+answer = random.randint(1,100)
+
+number = int(input('数字を当ててみてね！'))
+
+while answer != number:
+    if answer > number:
+        print('はずれ！もっと大きい数字だよ？ｗｗｗ')
+    if answer < number:
+        print('はずれ！もっと小さい数字？www')
+
+    number = int(input('数字を当ててみてね！'))
+print("ブラボー！正解です！")
