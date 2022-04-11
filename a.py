21ef36790d5f6d2e945f87b3b930f99b4b4569aa
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-04-11 12:57:48 +0900

    Update a.py

--- a.py
+++ a.py
@@ -7,7 +7,7 @@ while answer != number:
     if answer > number:
         print('はずれ！もっと大きい数字だよ？ｗｗｗ')
     if answer < number:
-        print('はずれ！もっと小さい数字？www')
+        print('はずれ！もっと小さい数字だよ？www')
 
     number = int(input('数字を当ててみてね！'))
 print("ブラボー！正解です！")
