138f96073cdfbb82093029e840f10e457f009ec2
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-06-01 15:53:23 +0900

    変更

--- sensor/motor/motor3.py
+++ sensor/motor/motor3.py
@@ -12,66 +12,50 @@ pi1.set_mode(AIN1, pigpio.OUTPUT)
 pi1.set_mode(AIN2, pigpio.OUTPUT)
 pi1.set_mode(PWMA,pigpio.OUTPUT)
 
-motor_prior_l = 0	#Left Motor Speed Prior
-motor_prior_r = 0	#Right Motor Speed Prior
+motor_prior_c = 0	#Motor Speed Prior
 
-def motor(left, right, t = 0.001, mode = 0):
-	global motor_prior_l
-	global motor_prior_r
-	motorPL = 0.0
-	motorPR = 0.0
+def motor(c, t = 0.001, mode = 0):
+	global motor_prior_c
+	motorPC = 0.0
 
 	#if motor wiring changed, check these val
-	left = left * (-1.0)
-	right = right * (-1.0)
+	center = center * (-1.0)
 
 	t1 = time.time()
 	while(time.time() - t1 < t):
 		if mode == 2:
-			while left + 10.0 < motor_prior_l:
-				motorPL = motor_prior_l + 3
-				motorPR = motor_prior_r + 3
-				motor_prior_l = motorPL
-				motor_prior_r = motorPR
-				motorPL = int(motorPL * 10000)
-				motorPR = int(motorPR * 10000)
+			while center + 10.0 < motor_prior_c:
+				motorPC = motor_prior_c + 3
+				motor_prior_c = motorPC
+				motorPC = int(motorPC * 10000)
 
 				pi1.write(AIN1, 1)
 				pi1.write(AIN2, 0)
 
 				#print(motorPL, motorPR)
-				pi1.hardware_PWM(PWMA, 500, abs(motorPL))
+				pi1.hardware_PWM(PWMA, 500, abs(motorPC))
 			mode = 0
 
-		#print(motor_prior_l, motor_prior_r)
-		if left < motor_prior_l:
-			motorPL = motor_prior_l  - 1
-		elif left > motor_prior_l:
-			motorPL = motor_prior_l + 1
+		#print(motor_prior_c)
+		if center < motor_prior_c:
+			motorPC = motor_prior_c  - 1
+		elif center > motor_prior_c:
+			moterPC = motor_prior_c + 1
 		else:
-			motorPL = motor_prior_l
+			motorPC = motor_prior_c
 
-		if right < motor_prior_r:
-			motorPR = motor_prior_r - 1
-		elif right > motor_prior_r:
-			motorPR = motor_prior_r + 1
-		else :
-			motorPR = motor_prior_r
+		motor_prior_c = motorPC
 
-		motor_prior_l = motorPL
-		motor_prior_r = motorPR
 		#print(str(motorPL) + "\t" + str(motorPR))
-		motorPL = int(motorPL * 10000)
-		motorPR = int(motorPR * 10000)
+		motorPC = int(motorPC * 10000)
 
 		if(mode == 1):
-			motorPL = int(left * 10000)
-			motorPR = int(right * 10000)
+			motorPL = int(center * 10000)
 
-		if motorPL > 0:
+		if motorPC > 0:
 			pi1.write(AIN1, 1)
 			pi1.write(AIN2, 0)
-		elif motorPL < 0:
+		elif motorPC < 0:
 			pi1.write(AIN1, 0)
 			pi1.write(AIN2, 1)
 		else:
@@ -79,18 +63,17 @@ def motor(left, right, t = 0.001, mode = 0):
 			pi1.write(AIN2, 0)
 
 		#print(motorPL, motorPR)
-		pi1.hardware_PWM(PWMA, 500, abs(motorPL))
+		pi1.hardware_PWM(PWMA, 500, abs(motorPC))
 
 		if(mode == 1):
 			time.sleep(t)
 		else:
 			time.sleep(0.005)
 
-	return [motorPL, motorPR]
+	return [motorPC]
 
 def motor_stop():
 	pi1.hardware_PWM(PWMA, 500, 0)
-	pi1.hardware_PWM(PWMB, 500, 0)
 
 if __name__ == "__main__":
 	try:
@@ -104,22 +87,17 @@ if __name__ == "__main__":
 		while 1:
 			try:
 				if f == 0:
-					L = float(input("input left motor "))
+					C = float(input("input center motor "))
 					f = 1
-					if L > 100 or L < -100:
-						f = 0
-				if f == 1:
-					R = float(input("input right motor "))
-					f = 2
-					if R > 100 or L < -100:
+					if C > 100 or C < -100:
 						f = 1
-				if f == 2:
+				if f == 1:
 					T = float(input("input time "))
-					f = 3
-				if f == 3:
+					f = 2
+				if f == 2:
 					M = float(input("input mode "))
 					if M == 0 or M == 1:
-						motor(L, R, T, M)
+						motor(C, T, M)
 						motor(0, 0, 2)
 						motor_stop()
 						f = 0
