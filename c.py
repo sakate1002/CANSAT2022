6e232084e1bf6aa290f00a29f05ef7de80b681ad
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-06-02 20:10:47 +0900

    変更

--- c.py
+++ c.py
@@ -1,7 +1,9 @@
+import time
 from xbee import str_trans
 from gps import open_gps , read_gps , close_gps
 open_gps()
 while True :
     a = read_gps()
     str_trans(a)
+    time.sleep(1)
 close_gps()
