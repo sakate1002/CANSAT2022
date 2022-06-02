637400bc7c6e259fa84dd98d6611661d7784f8ac
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-06-02 19:55:36 +0900

    変更

--- c.py
+++ c.py
@@ -1,5 +1,7 @@
 from xbee import str_trans
-from bme280 import bme280_read , bme280_setup
+from bme280 import bme280_read , bme280_setup , writeReg , bme280_calib_param
+#writeReg()
 bme280_setup()
+bme280_calib_param()
 a = bme280_read()
 str_trans(a)
