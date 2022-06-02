979d30ca90199bf2dfe328b0665689ebb32df389
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-06-02 20:09:13 +0900

    変更

--- c.py
+++ c.py
@@ -1,7 +1,7 @@
 from xbee import str_trans
 from gps import open_gps , read_gps , close_gps
 open_gps()
-a = read_gps()
 while True :
+    a = read_gps()
     str_trans(a)
-    close_gps()
+close_gps()
