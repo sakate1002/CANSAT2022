b8772f694a6f84073cb03aaa71493b77daa0b13b
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-06-06 13:52:36 +0900

    あ

---
+++ calibration.py
@@ -0,0 +1,239 @@
+# --- must be installed module ---#import numpy as np
+import math
+import time
+import traceback
+import datetime
+import numpy as np
+
+import bmx055
+import other
+from bmx055 import bmx055_setup
+import gps
+import gps_navigate
+from other import print_xbee
+import motor
+import stuck
+
+
+path_log = '/home/pi/Desktop/cansat2021/log/calibration.txt'
+
+
+# filecount = len(glob.glob1(path_log, '*' + '.txt'))
+
+# Calibration_rotate_controlLog = '/home/pi/log/Calibration_rotate_controlLog.txt'
+
+
+def get_data():
+    """
+        MBC050からデータを得る
+        """
+    try:
+        magData = bmx055.mag_dataRead()
+    except KeyboardInterrupt:
+        print()
+    except Exception as e:
+        print()
+        print(e)
+    # --- get magnet sensor data ---#
+    magx = magData[0]
+    magy = magData[1]
+    magz = magData[2]
+    return magx, magy, magz
+
+
+def get_data_offset(magx_off, magy_off, magz_off):
+    """
+        MBC050からオフセットを考慮して磁気データを得る関数
+        """
+    try:
+        magData = bmx055.mag_dataRead()
+    except KeyboardInterrupt:
+        print()
+    except Exception as e:
+        print()
+        print(e)
+    # --- get magnet sensor data ---#
+    magx = magData[0] - magx_off
+    magy = magData[1] - magy_off
+    magz = magData[2] - magz_off
+    return magx, magy, magz
+
+
+def magdata_matrix(l, r, n):
+    """
+        キャリブレーション用の地磁気データを得るための関数。
+        モータを連続的に動かして回転して地磁気データを得る。
+        """
+    try:
+        stuck.ue_jug()
+        magx, magy, magz = get_data()
+        magdata = np.array([[magx, magy, magz]])
+        for _ in range(n - 1):
+            motor.motor_continue(l, r)
+            magx, magy, magz = get_data()
+            print(magx, magy)
+            # --- multi dimention matrix ---#
+            magdata = np.append(magdata, np.array(
+                [[magx, magy, magz]]), axis=0)
+            time.sleep(0.03)
+        motor.deceleration(l, r)
+    except KeyboardInterrupt:
+        print('Interrupt')
+    except Exception as e:
+        print(e.message())
+    return magdata
+
+
+def magdata_matrix_hand():
+    """
+        キャリブレーション用の磁気値を手持ちで得るための関数
+        """
+    try:
+        magx, magy, magz = get_data()
+        magdata = np.array([[magx, magy, magz]])
+        for i in range(60):
+            print('少し回転')
+            time.sleep(1)
+            print(f'{i + 1}回目')
+            magx, magy, magz = get_data()
+            # --- multi dimention matrix ---#
+            magdata = np.append(magdata, np.array(
+                [[magx, magy, magz]]), axis=0)
+    except KeyboardInterrupt:
+        print('Interrupt')
+    except Exception as e:
+        print(e.message())
+    return magdata
+
+
+def magdata_matrix_offset(l, r, t, magx_off, magy_off, magz_off):
+    """
+        オフセットを考慮したデータセットを取得するための関数
+        """
+    try:
+        magx, magy, magz = get_data_offset(magx_off, magy_off, magz_off)
+        magdata = np.array([[magx, magy, magz]])
+        for _ in range(20):
+            motor(l, r, t)
+            magx, magy, magz = get_data_offset(magx_off, magy_off, magz_off)
+            # --- multi dimention matrix ---#
+            magdata = np.append(magdata, np.array(
+                [[magx, magy, magz]]), axis=0)
+    except KeyboardInterrupt:
+        print_xbee('Interrupt')
+    except Exception as e:
+        print_xbee(e.message())
+    return magdata
+
+
+def calculate_offset(magdata):
+    """
+    オフセットを計算する関数
+    """
+    # --- manage each element sepalately ---#
+    magx_array = magdata[:, 0]
+    magy_array = magdata[:, 1]
+    magz_array = magdata[:, 2]
+
+    # --- find maximam gps value and minimam gps value respectively ---#
+    magx_max = magx_array[np.argmax(magx_array)]
+    magy_max = magy_array[np.argmax(magy_array)]
+    magz_max = magz_array[np.argmax(magz_array)]
+
+    magx_min = magx_array[np.argmin(magx_array)]
+    magy_min = magy_array[np.argmin(magy_array)]
+    magz_min = magz_array[np.argmin(magz_array)]
+
+    # --- calucurate offset ---#
+    magx_off = (magx_max + magx_min) / 2
+    magy_off = (magy_max + magy_min) / 2
+    magz_off = (magz_max + magz_min) / 2
+
+    # --- save offset --- #
+    other.log('/home/pi/Desktop/cansat2021/log/calibrationLog.txt',
+              datetime.datetime.now(), magx_off, magy_off)
+
+    return magx_array, magy_array, magz_array, magx_off, magy_off, magz_off
+
+
+def cal(l, r, n):
+    magdata = magdata_matrix(l, r, n)
+    _, _, _, magx_off, magy_off, _ = calculate_offset(magdata)
+    return magx_off, magy_off
+
+
+def angle(magx, magy, magx_off=0, magy_off=0):
+    if magy - magy_off == 0:
+        magy += 0.000001
+    θ = math.degrees(math.atan((magy - magy_off) / (magx - magx_off)))
+
+    if magx - magx_off > 0 and magy - magy_off > 0:  # First quadrant
+        pass  # 0 <= θ <= 90
+    elif magx - magx_off < 0 and magy - magy_off > 0:  # Second quadrant
+        θ = 180 + θ  # 90 <= θ <= 180
+    elif magx - magx_off < 0 and magy - magy_off < 0:  # Third quadrant
+        θ = θ + 180  # 180 <= θ <= 270
+    elif magx - magx_off > 0 and magy - magy_off < 0:  # Fourth quadrant
+        θ = 360 + θ  # 270 <= θ <= 360
+
+    θ += 90
+    if 360 <= θ <= 450:
+        θ -= 360
+    return θ
+
+
+def calculate_direction(lon2, lat2):
+    # --- read gps data ---#
+    try:
+        gps.open_gps()
+        utc, lat, lon, sHeight, gHeight = gps.gps_data_read()
+        lat1 = lat
+        lon1 = lon
+    except KeyboardInterrupt:
+        gps.close_gps()
+        print_xbee("\r\nKeyboard Intruppted, Serial Closed")
+    except:
+        gps.close_gps()
+        print_xbee(traceback.format_exc())
+    # --- calculate angle to goal ---#
+    direction = gps_navigate.vincenty_inverse(lat1, lon1, lat2, lon2)
+    return direction
+
+
+if __name__ == "__main__":
+    an = int(input("aaaaaa"))
+    motor.setup()
+    bmx055.bmx055_setup()
+    magdata =magdata_matrix(an, -an, 1000)
+
+
+    # try:
+    #     r = float(input('右の出力は？'))
+    #     l = float(input('左の出力は？'))
+    #     t = float(input('一回の回転時間は？'))
+    #     # n = int(input("取得するデータ数は？"))
+    #     # --- setup ---#
+    #     mag.bmc050_setup()
+    #     t_start = time.time()
+    #     # --- calibration ---#
+    #     magdata_Old = magdata_matrix(l, r, t)
+    #     # --- calculate offset ---#
+    #     magx_array_Old, magy_array_Old, magz_array_Old, magx_off, magy_off, magz_off = calculate_offset(
+    #         magdata_Old)
+    #     time.sleep(0.1)
+    #     # ----Take magnetic data considering offset----#
+    #     magdata_new = magdata_matrix_offset(
+    #         l, r, t, magx_off, magy_off, magz_off)
+    #     magx_array_new = magdata_new[:, 0]
+    #     magy_array_new = magdata_new[:, 1]
+    #     magz_array_new = magdata_new[:, 2]
+    #     for i in range(len(magx_array_new)):
+    #         other.log(
+    #             path_log, magx_array_Old[i], magy_array_Old[i], magx_array_new[i], magy_array_new[i])
+    #     print_xbee("success")
+
+    # except KeyboardInterrupt:
+    #     print_xbee("Interrupted")
+
+    # finally:
+    #     print_xbee("End")
---
+++ gps_running.py
@@ -0,0 +1,219 @@
+import datetime
+import time
+
+import gps_navigate
+import gps
+import bmx055
+import motor
+import motor3
+import xbee
+import calibration
+import stuck
+import other
+
+
+def angle_goal(magx_off, magy_off, lon2, lat2):
+    """
+    ゴールとの相対角度を算出する関数
+
+    -180~180度
+    """
+    magdata = bmx055.mag_dataRead()
+    mag_x = magdata[0]
+    mag_y = magdata[1]
+    theta = calibration.angle(mag_x, mag_y, magx_off, magy_off)
+    direction = calibration.calculate_direction(lon2, lat2)
+    azimuth = direction["azimuth1"]
+    angle_relative = azimuth - theta
+    if angle_relative >= 0:
+        angle_relative = angle_relative if angle_relative <= 180 else angle_relative - 360
+    else:
+        angle_relative = angle_relative if angle_relative >= -180 else angle_relative + 360
+    return angle_relative
+
+
+def adjust_direction(theta, magx_off, magy_off, lon2, lat2):
+    """
+    方向調整
+    """
+
+    # print('ゴールとの角度theta = ' + str(theta) + '---回転調整開始！')
+    # stuck.ue_jug()
+    # an = 30
+    # if 45 < theta <= 180 :
+    #     motor.motor_continue(an, -an)
+    # elif -180 < theta < -45:
+    #     motor.motor_continue(-an, an)
+    # elif 0 <= theta <= 45:
+    #     motor.deceleration(an, -an)
+    # elif -45 <= theta <= 0:
+    #     motor.motor_continue(-an, an)
+
+    # theta = angle_goal(magx_off, magy_off, lon2, lat2)
+    # print('Calculated angle_relative: {theta}')
+    # time.sleep(0.03)
+
+    stuck_count = 1
+    t_small = 0.1
+    t_big = 0.2
+    force = 35
+    while 30 < theta <= 180 or -180 < theta < -30:
+        if stuck_count >= 16:
+            ##方向調整が不可能な場合はスタックしたとみなして、もう一度キャリブレーションからスタート##
+            other.print_xbee(
+                "!!!!can't ajdust direction.   start stuck avoid!!!!!")
+            stuck.stuck_avoid()
+            magx_off, magy_off = calibration.cal(40, -40, 30)
+            stuck_count = -1
+        if stuck_count % 7 == 0:
+            other.print_xbee('Increase output')
+            force += 10
+        if 30 <= theta <= 60:
+            other.print_xbee(
+                f'theta = {theta}\t---rotation_ver1 (stuck:{stuck_count})')
+            motor.move(force, -force, t_small)
+
+        elif 60 < theta <= 180:
+            other.print_xbee(
+                f'theta = {theta}\t---rotation_ver2 (stuck:{stuck_count})')
+            motor.move(force, -force, t_big)
+
+        elif -60 <= theta <= -30:
+            other.print_xbee(
+                f'theta = {theta}\t---rotation_ver3 (stuck:{stuck_count})')
+            motor.move(-force, force, t_small)
+        elif -180 < theta < -60:
+            other.print_xbee(
+                f'theta = {theta}\t---rotation_ver4 (stuck:{stuck_count})')
+            motor.move(-force, force, t_big)
+        else:
+            print(f'theta = {theta}')
+
+        stuck_count += 1
+        stuck.ue_jug()
+        theta = angle_goal(magx_off, magy_off, lon2, lat2)
+        print('Calculated angle_relative: {theta}')
+        time.sleep(1)
+    other.print_xbee(f'theta = {theta} \t rotation finished!!!')
+
+
+def drive(lon2, lat2, thd_distance, t_adj_gps, logpath='/home/pi/Desktop/cansat2021ver/log/gpsrunningLog', t_start=0):
+    """
+    GPS走行の関数
+    統合する場合はprintをXbee.str_transに変更，other.saveLogのコメントアウトを外す
+    """
+    direction = calibration.calculate_direction(lon2, lat2)
+    goal_distance = direction['distance']
+    while goal_distance >= thd_distance:
+        t_stuck_count = 1
+        stuck.ue_jug()
+
+        # ------------- calibration -------------#
+        # xbee.str_trans('calibration Start')
+        other.print_xbee('##--calibration Start--##\n')
+        magx_off, magy_off = calibration.cal(40, -40, 30)
+        print(f'magx_off: {magx_off}\tmagy_off: {magy_off}\n')
+
+        theta = angle_goal(magx_off, magy_off, lon2, lat2)
+        adjust_direction(theta, magx_off, magy_off, lon2, lat2)
+
+        t_cal = time.time()
+        lat_old, lon_old = gps.location()
+        while time.time() - t_cal <= t_adj_gps:
+            lat1, lon1 = gps.location()
+            lat_new, lon_new = lat1, lon1
+            direction = gps_navigate.vincenty_inverse(lat1, lon1, lat2, lon2)
+            azimuth, goal_distance = direction["azimuth1"], direction["distance"]
+            other.print_xbee(
+                f'lat: {lat1}\tlon: {lon1}\tdistance: {goal_distance}\tazimuth: {azimuth}\n')
+
+            if t_stuck_count % 8 == 0:
+                ##↑何秒おきにスタックジャッジするかを決める##
+                if stuck.stuck_jug(lat_old, lon_old, lat_new, lon_new, 4):
+                    pass
+                else:
+                    stuck.stuck_avoid()
+                    pass
+                lat_old, lon_old = gps.location()
+
+            if goal_distance <= thd_distance:
+                break
+            else:
+                for _ in range(25):
+                    magdata = bmx055.mag_read()
+                    mag_x = magdata[0]
+                    mag_y = magdata[1]
+
+                    theta = calibration.angle(mag_x, mag_y, magx_off, magy_off)
+                    angle_relative = azimuth - theta
+                    if angle_relative >= 0:
+                        angle_relative = angle_relative if angle_relative <= 180 else angle_relative - 360
+                    else:
+                        angle_relative = angle_relative if angle_relative >= -180 else angle_relative + 360
+                    theta = angle_relative
+                    adj_r = 0
+                    # if theta >= 0:
+                    #     if theta <= 8:
+                    #         adj = 0
+                    #     elif theta <= 15:
+                    #         adj = 5
+                    #     elif theta <= 90:
+                    #         adj = 20
+                    #         adj_r = 5
+                    #     else:
+                    #         adj = 30
+                    #         adj_r = 5
+                    # else:
+                    #     if theta >= - 8:
+                    #         adj = 0
+                    #     elif theta >= -15:
+                    #         adj = -10
+                    #     elif theta >= -90:
+                    #         adj = -20
+                    #     else:
+                    #         adj = -30
+                    if theta >= 0:
+                        if theta <= 15:
+                            adj = 0
+                        elif theta <= 90:
+                            adj = 20
+                            adj_r = 5
+                        else:
+                            adj = 30
+                            adj_r = 5
+                    else:
+                        if theta >= -15:
+                            adj = 0
+                        elif theta >= -90:
+                            adj = -20
+                        else:
+                            adj = -30
+                    print(f'angle ----- {theta}')
+                    strength_l, strength_r = 70 + adj, 70 - adj - adj_r
+                    motor.motor_continue(strength_l, strength_r)
+                    time.sleep(0.04)
+            t_stuck_count += 1
+            other.log(logpath, datetime.datetime.now(), time.time() -
+                      t_start, lat1, lon1, direction['distance'], angle_relative)
+        motor.deceleration(strength_l, strength_r)
+        time.sleep(2)
+        lat_new, lon_new = gps.location()
+
+        direction = calibration.calculate_direction(lon2, lat2)
+        goal_distance = direction['distance']
+        other.print_xbee(f'-----distance: {goal_distance}-----')
+
+
+if __name__ == '__main__':
+    # lat2 = 35.918548
+    # lon2 = 139.908896
+    # lat2 = 35.9234892
+    # lon2 = 139.9118744
+    lat2, lon2 = 35.921247, 139.910953
+    lat2 = 35.412957
+    lon2 = 138.592717
+    gps.open_gps()
+    bmx055.bmc050_setup()
+    motor.setup()
+
+    drive(lon2, lat2, thd_distance=10, t_adj_gps=60)
