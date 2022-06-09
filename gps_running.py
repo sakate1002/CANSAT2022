a8778b4346c82f188a66a39582504ba0e090f82f
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-06-09 18:25:27 +0900

    変更

--- gps_running.py
+++ gps_running.py
@@ -113,7 +113,7 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath='/home/cansat2022/Desktop
         goal_distance = direction['distance']
         mission_count = 0
         while mission_count < 1:
-            if (mission_distance - 10) < goal_distance < (mission_distance + 10):
+            if (mission_distance - 10) < mission_distance < (mission_distance + 10):
             #ミッション用の関数呼び出す!!阪手
                 mission_count += 1
         
@@ -139,7 +139,7 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath='/home/cansat2022/Desktop
 
                     if t_stuck_count % 8 == 0:
                     ##↑何秒おきにスタックジャッジするかを決める##
-                        if stuck.stuck_jug(lat_old, lon_old, lat_new, lon_new, 4):
+                        if stuck.stuck_jug(lat_old, lon_old, lat_new, lon_new, 4): #スタックの基準（閾値は調整）サカイェ
                             pass
                         else:
                             stuck.stuck_avoid()
@@ -203,8 +203,7 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath='/home/cansat2022/Desktop
                             motor.motor_continue(strength_l, strength_r)
                             time.sleep(0.04)
             t_stuck_count += 1
-            other.log(logpath, datetime.datetime.now(), time.time() -
-                      t_start, lat1, lon1, direction['distance'], angle_relative)
+            other.log(logpath, datetime.datetime.now(), time.time() - t_start, lat1, lon1, direction['distance'], angle_relative)
         motor.deceleration(strength_l, strength_r)
         time.sleep(2)
         lat_new, lon_new = gps.location()
@@ -213,6 +212,99 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath='/home/cansat2022/Desktop
         goal_distance = direction['distance']
         other.print_xbee(f'-----distance: {goal_distance}-----')
 
+            #ミッション後
+            # ------------- calibration -------------#
+            # xbee.str_trans('calibration Start')
+        other.print_xbee('##--calibration Start--##\n')
+        magx_off, magy_off = calibration.cal(40, -40, 30)
+        print(f'magx_off: {magx_off}\tmagy_off: {magy_off}\n')
+        theta = angle_goal(magx_off, magy_off, lon2, lat2)
+        adjust_direction(theta, magx_off, magy_off, lon2, lat2)
+
+        t_cal = time.time()
+        lat_old, lon_old = gps.location()
+        while time.time() - t_cal <= t_adj_gps:
+            lat1, lon1 = gps.location()
+            lat_new, lon_new = lat1, lon1
+            direction = gps_navigate.vincenty_inverse(lat1, lon1, lat2, lon2)
+            azimuth, goal_distance = direction["azimuth1"], direction["distance"]
+            other.print_xbee(
+                f'lat: {lat1}\tlon: {lon1}\tdistance: {goal_distance}\tazimuth: {azimuth}\n')
+
+            if t_stuck_count % 8 == 0:
+                    ##↑何秒おきにスタックジャッジするかを決める##
+                        if stuck.stuck_jug(lat_old, lon_old, lat_new, lon_new, 4):
+                            pass
+                        else:
+                            stuck.stuck_avoid()
+                            pass
+                        lat_old, lon_old = gps.location()
+
+            if goal_distance <= thd_distance:
+                break
+            else:
+                for _ in range(25):
+                    magdata = bmx055.mag_dataRead()
+                    mag_x = magdata[0]
+                    mag_y = magdata[1]
+
+                    theta = calibration.angle(mag_x, mag_y, magx_off, magy_off)
+                    angle_relative = azimuth - theta
+                    if angle_relative >= 0:
+                        angle_relative = angle_relative if angle_relative <= 180 else angle_relative - 360
+                    else:
+                        angle_relative = angle_relative if angle_relative >= -180 else angle_relative + 360
+                    theta = angle_relative
+                    adj_r = 0
+                    # if theta >= 0:
+                    #     if theta <= 8:
+                    #         adj = 0
+                    #     elif theta <= 15:
+                    #         adj = 5
+                    #     elif theta <= 90:
+                    #         adj = 20
+                    #         adj_r = 5
+                    #     else:
+                    #         adj = 30
+                    #         adj_r = 5
+                    # else:
+                    #     if theta >= - 8:
+                    #         adj = 0
+                    #     elif theta >= -15:
+                    #         adj = -10
+                    #     elif theta >= -90:
+                    #         adj = -20
+                    #     else:
+                    #         adj = -30
+                    if theta >= 0:
+                        if theta <= 15:
+                            adj = 0
+                        elif theta <= 90:
+                            adj = 20
+                            adj_r = 5 #右のモーターだけ？阪手
+                        else:
+                            adj = 30
+                            adj_r = 5
+                    else:
+                        if theta >= -15:
+                            adj = 0
+                        elif theta >= -90:
+                            adj = -20
+                        else:
+                            adj = -30
+                    print(f'angle ----- {theta}')
+                    strength_l, strength_r = 70 + adj, 70 - adj - adj_r
+                    motor.motor_continue(strength_l, strength_r)
+                    time.sleep(0.04)
+            t_stuck_count += 1
+            other.log(logpath, datetime.datetime.now(), time.time() - t_start, lat1, lon1, direction['distance'], angle_relative)
+        motor.deceleration(strength_l, strength_r)
+        time.sleep(2)
+        lat_new, lon_new = gps.location()
+
+        direction = calibration.calculate_direction(lon2, lat2)
+        goal_distance = direction['distance']
+        other.print_xbee(f'-----distance: {goal_distance}-----')
 
 if __name__ == '__main__':
     # lat2 = 35.918548
