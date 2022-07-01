8a1a1718751731c29bf5647a2eecadb38891548c
ryou0929 <sugimoto@kimura-lab.net>
2022-07-01 15:16:58 +0900

    addddd

--- m_motor.py
+++ m_motor.py
@@ -1,7 +1,10 @@
 import pigpio
+import picamera
 import time
 import traceback
 import sys
+import os
+
 
 AIN1 = 13
 AIN2 = 19
@@ -74,6 +77,72 @@ def motor(center, t = 0.001, mode = 0):
 
 def motor_stop():
 	pi1.hardware_PWM(PWMA, 500, 0)
+
+
+def picture(path, width=320, height=240):
+    
+    #/dir/dir/dir/fileの時にfileの前にディレクトリが存在するか調べる関数
+    #引数は/dir/dir/dir/fileの形のパス
+    def dir(path):
+        fd = path.rfind('/')
+        dir = path[:fd]
+        is_dir = os.path.isdir(dir)
+        return is_dir
+    
+    #dir関数で調べた結果ディレクトリが存在しない場合はそのディレクトリを作成する
+    def make_dir(path):
+        if not dir(path):
+            fd = path.rfind('/')
+            directory = path[:fd]
+            os.mkdir(directory)
+            print('******Directory is maked******')
+        else:
+            print('**Directory is exist**')
+    
+    #ファイル名に番号をつけるための関数
+    #引数f:つけたいファイル名
+    #引数ext:ファイルの拡張子
+    #戻り値f:ファイル名+0000.拡張子
+    #戻り値の番号は増えていく
+    def filename(f, ext):
+        i = 0
+        while 1:
+            num = ""
+            if len(str(i)) <= 4:
+                for j in range(4 - len(str(i))):
+                    num = num + "0"
+                num = num + str(i)
+            else:
+                num = str(i)
+            if not (os.path.exists(f + num + "." + ext)):
+                break
+            i = i + 1
+        f = f + num + "." + ext
+        return f
+
+    try:
+        make_dir(path)
+        with picamera.PiCamera() as camera:
+            #取得した画像の回転
+            camera.rotation = 270
+            #取得する画像の解像度を設定→どのような基準で設定するのか
+            #使用するカメラの解像度は静止画解像度で3280×2464
+            camera.resolution = (width, height)
+            #指定したパスを持つファイルを作成
+            filepath = filename(path, 'jpg')
+            #そのファイルに取得した画像を入れる
+            camera.capture(filepath)
+    #パスが切れているときはNULL
+    except picamera.exc.PiCameraMMALError:
+        filepath = "Null"
+        time.sleep(0.8)
+    #そのほかのエラーの時はNULL
+    except:
+        print(traceback.format_exc())
+        time.sleep(0.1)
+        filepath = "Null"
+    return filepath
+
 
 if __name__ == "__main__":
 	try:
