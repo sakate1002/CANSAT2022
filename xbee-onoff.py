a8188909d65aeb4423559414bdf33f6440d1ac16
ryou0929 <sugimoto@kimura-lab.net>
2022-07-13 16:28:01 +0900

    aedd

--- xbee-onoff.py
+++ xbee-onoff.py
@@ -1,12 +1,12 @@
 from xbee import on,off,str_trans
 import time
 while 1:
-    str_trans("Bye")
-    on()
-    str_trans("Hello")
-    print("on")
-    time.sleep(2)
     off()
     print("off")
     time.sleep(2)
+    str_trans("Bye")
+    on()
+    print("on")
+    time.sleep(2)
+    str_trans("Hello")
 # off()
