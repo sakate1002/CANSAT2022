7b5686c7ed4693650e78d9a7ea5b02e9b6088ced
ryou0929 <sugimoto@kimura-lab.net>
2022-07-13 16:25:36 +0900

    add

--- xbee-onoff.py
+++ xbee-onoff.py
@@ -1,15 +1,9 @@
 from xbee import on,off,str_trans
 import time
 while 1:
-    str_trans("Hello")
-    str_trans("Hello")
-    str_trans("Hello")
-    str_trans("Hello")
+    str_trans("Bye")
     on()
-    str_trans("bye!")
-    str_trans("Bye!")
-    str_trans("Bye!")
-    str_trans("Bye!")
+    str_trans("Hello")
     print("on")
     time.sleep(2)
     off()
