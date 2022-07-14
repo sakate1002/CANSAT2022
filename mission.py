c49f8c044bbfceb0babd07053788962d2bd74225
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-07-14 15:33:20 +0900

    ミッション

--- mission.py
+++ mission.py
@@ -155,6 +155,7 @@ def mission():
 if __name__ == "__main__":
     try:
         for i in range(4):
+            time.sleep(2.0)
             photoName = picture('photo/photo', 320, 240)
             motor(-60, 0.4, 1)
             motor_stop()
