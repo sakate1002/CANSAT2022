282fa726113090576792027c2f06c068cf3ecbdb
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-07-14 15:27:02 +0900

    ミッション

--- mission.py
+++ mission.py
@@ -153,13 +153,18 @@ def mission():
 
 
 if __name__ == "__main__":
-    
-    for i in range(4):
-        photoName = picture('photo/photo', 320, 240)
-        motor(-60, 0.4, 1)
-        motor_stop()
-        time.sleep(2.0)
-        photoName = picture('photo/photo', 320, 240)
-        time.sleep(2.0)
-        motor(45, 0.3, 1)
-        motor_stop()
+    try:
+        for i in range(4):
+            photoName = picture('photo/photo', 320, 240)
+            motor(-60, 0.4, 1)
+            motor_stop()
+            time.sleep(2.0)
+            photoName = picture('photo/photo', 320, 240)
+            time.sleep(2.0)
+            motor(45, 0.3, 1)
+            motor_stop()
+            time.sleep(2.0)
+    except Exception as e:
+        exit()       
+
+        
