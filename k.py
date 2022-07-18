a4e293bb9531d38b8a657d435b6ba42a98781baa
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-07-18 16:37:37 +0900

    変更

--- k.py
+++ k.py
@@ -1,6 +1,11 @@
+import gps
+import bmx055
 import calibration
+
+gps.open_gps()
+bmx055.bmx055_setup()
 lat2 = 35.9237822
 lon2 = 139.91122508
 direction = calibration.calculate_direction(lon2, lat2)
 goal_distance = direction['distance']
-print(goal_distance)
+print(int(goal_distance))
