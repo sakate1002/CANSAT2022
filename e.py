d2e62da65c6793466ee6879442cc7ceffeda668e
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-07-20 13:28:44 +0900

    変更

--- e.py
+++ e.py
@@ -58,8 +58,8 @@ number_data = 30
 # variable for GPSrun
 # lat2 = 35.918548
 # lon2 = 139.908896
-lat2 = 35.9240157
-lon2 = 139.9112665
+lat2 = 35.9237070
+lon2 = 139.9134495
 
 th_distance = 5
 t_adj_gps = 20
