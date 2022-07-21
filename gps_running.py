2c0386cca9eef98738d961a7ad3098b7e53520b6
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-07-21 12:42:44 +0900

    変更

--- gps_running.py
+++ gps_running.py
@@ -175,9 +175,9 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath='/home/cansat2022/CANSAT2
                     time.sleep(0.04)
             t_stuck_count += 1
             other.log(logpath, datetime.datetime.now(), time.time() - t_start, lat1, lon1, direction['distance'], angle_relative)
-            motor.deceleration(strength_l, strength_r)
-            time.sleep(2)
-            lat_new, lon_new = gps.location()
+        motor.deceleration(strength_l, strength_r)
+        #time.sleep(2)
+        lat_new, lon_new = gps.location()
 
         direction = calibration.calculate_direction(lon2, lat2)
         goal_distance = direction['distance']
