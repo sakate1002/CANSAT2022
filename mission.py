1cd6e1aad96feefb18996070a6b0f060096a87a1
sakate1002 <103055126+sakate1002@users.noreply.github.com>
2022-07-22 14:48:31 +0900

    ミッション

--- mission.py
+++ mission.py
@@ -148,7 +148,7 @@ def mission():
     motor(-30, 0.4, 1)
     motor_stop()
     photoName = picture('photo/photo', 320, 240)
-    motor(11, 0.4, 1)
+    motor(13, 0.4, 1)
     motor_stop()
 
 
@@ -162,7 +162,7 @@ if __name__ == "__main__":
             time.sleep(2.0)
             #photoName = picture('photo/photo', 320, 240)
             time.sleep(2.0)
-            motor(11, 0.4, 1)
+            motor(13, 0.4, 1)
             motor_stop()
             time.sleep(2.0)
     except KeyboardInterrupt:
